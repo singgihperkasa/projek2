d205b65688eec327e07f75e66b95e2e1e2dea7f5
singgihperkasa <76107369+singgihperkasa@users.noreply.github.com>
2025-11-16 05:57:59 +0700

    Delete deepseek_javascript_20251115_9aa92f (1).js

--- deepseek_javascript_20251115_9aa92f (1).js
+++
@@ -1,9 +0,0 @@
-async function loadSurahFromAPI(surahNumber) {
-    const response = await fetch(`https://api.quran.com/api/v4/verses/by_chapter/${surahNumber}?language=id&words=true`);
-    const data = await response.json();
-    return data.verses.map(verse => ({
-        number: verse.verse_number,
-        arabic: verse.text_uthmani,
-        translation: verse.translations[0]?.text || 'Terjemahan tidak tersedia'
-    }));
-}
