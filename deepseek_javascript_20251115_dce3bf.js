0cbe02a088f9baa63c52371fc9cdb585473e75d7
singgihperkasa <76107369+singgihperkasa@users.noreply.github.com>
2025-11-16 05:46:39 +0700

    Delete deepseek_javascript_20251115_dce3bf.js

--- deepseek_javascript_20251115_dce3bf.js
+++
@@ -1,4 +0,0 @@
-// Urutan playback:
-playCurrent() → 
-  if (isPlayingQari) → playQariAudio() → onEnded → isPlayingQari = false → playNextSegment()
-  if (!isPlayingQari) → playTranslationTTS() → onEnded → isPlayingQari = true → currentVerseIndex++ → playNextSegment()
